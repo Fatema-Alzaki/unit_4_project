bf423278aea9920617775461891a86dc234fa8ba
fatema <fatimaalzaki242@gmail.com>
2025-09-17 17:28:55 +0300

    wowowow

--- FabriQueue/settings.py
+++ FabriQueue/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-9)e!-rjesoe+=6dozmuac$8y^-+@kaqp7iy+tgiby^wtqqdc++
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
